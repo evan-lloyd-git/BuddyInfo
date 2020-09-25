af7f9638bf6ba826c44c3eee3d4d146e0ab47d86
evan-lloyd-git <evan-lloyd@users.noreply.github.com>
2020-09-24 20:45:37 -0400

    Added new message saying what the class is.

--- BuddyInfo/src/AddressBook.java
+++ BuddyInfo/src/AddressBook.java
@@ -11,12 +11,6 @@ public class AddressBook {
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
 
     }
-    public static void main(String[] args){
-        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
-        AddressBook addressBook = new AddressBook();
-        addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
-    }
     public void addBuddy(BuddyInfo buddy){
         book.add(buddy);
     }
